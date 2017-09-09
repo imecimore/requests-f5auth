1f4437ce4de675c09ed355dafa550251ef44e1a5
Ivan Mecimore <i.mecimore@f5.com>
2017-09-08 17:10:03 -0700

    Add tests for checking exceptions are thrown appropriately

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,7 +1,10 @@
 import unittest
 
+import requests
 from requests import Request, Session
 from requests_f5auth import XF5Auth
+from requests_f5auth.exceptions import F5AuthenticationError
+
 
 
 class XF5AuthTest(unittest.TestCase):
@@ -10,14 +13,14 @@ class XF5AuthTest(unittest.TestCase):
         self.bigiq_addr = '10.145.196.234'
         self.username = 'admin'
         self.password = 'f5site02'
+        self.url = 'https://' + self.bigiq_addr + '/mgmt/shared/echo'
 
     def test_username_password(self):
-        url = 'https://' + self.bigiq_addr + '/mgmt/shared/echo'
         auth = XF5Auth(self.username, self.password)
-        r = Request('GET', url, auth=auth).prepare()
+        r = Request('GET', self.url, auth=auth).prepare()
 
         # ensure url didn't get messed up
-        self.assertEqual(r.url, url)
+        self.assertEqual(r.url, self.url)
         self.assertTrue(r.headers.get('X-F5-Auth-Token', ''))
 
         s = Session()
@@ -26,9 +29,28 @@ class XF5AuthTest(unittest.TestCase):
         self.assertEquals(resp.status_code, 200)
 
     def test_refresh_token(self):
-        login = 'https://'
+        pass
 
 
     def test_access_token(self):
         pass
+
+    def test_fail_login(self):
+        auth = XF5Auth(self.username, self.password + 'NOT RIGHT')
+
+        with self.assertRaises(F5AuthenticationError) as cm:
+            r = Request('GET', self.url, auth=auth).prepare()
+
+    def test_bad_refresh_token(self):
+        auth = XF5Auth(refresh_token='abc')
+
+        with self.assertRaises(F5AuthenticationError) as cm:
+            r = Request('GET', self.url, auth=auth).prepare()
+
+
+    def test_bad_access_token(self):
+        auth = XF5Auth(access_token='abc')
+        resp = requests.get(self.url, auth=auth, verify=False)
+        self.assertEquals(resp.status_code, 401)
+
 
